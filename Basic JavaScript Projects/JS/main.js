c5eb13254c05b12eaa738dc9daed8e8289c3865a
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2022-07-19 09:32:30 -0300

    Add files via upload

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -2,4 +2,22 @@
 //document.write("Hello, world!");
 //var A = "This is a dummy assignment!";
 //window.alert(A);
-document.write("Brian told May: \"We are the champions, my friend! And no time for loosers\"<br>\"Fear of the dark!\" May responded.");
+//document.write("Brian told May: \"We are the champions, my friend! And no time for loosers\"<br>\"Fear of the dark!\" May responded.");
+
+/* document.write("\"My son ask for thyself another kingdom"
++ "\"For that which I leave is too small for thee"
++ "Near to the east"
++ "In a part of ancient Greece"
++ "In an ancient land called Macedonia"
++ "Was born a son"
++ "To Philip of Macedon"
++ "The legend, his name was Alexander"
++ "At the age of nineteen"
++ "He became the Macedon King"
++ "And he swore to free all of Asia Minor"
++ "By the Aegean Sea"
++ "In 334 B.C"
++ "He utterly beat the armies of Persia"); */
+
+var Singer = "Bruce Dickinson", Lead_Guitar = "Dave Murray", Guitar2 = "Adrian Smith", Guitar3 = "Janick Gers", Bass_Guitar = "Steve Harris", Drums = "Nicko McBrain";
+document.write (Bass_Guitar);
