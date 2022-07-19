5620cb5de7304ff50fd9de3e4a44e3385521cc58
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2022-07-19 09:02:20 -0300

    Add files via upload

--- Basic JavaScript Projects/JS/main.js
+++ Basic JavaScript Projects/JS/main.js
@@ -1,4 +1,5 @@
 // window.alert("Hello, World!");
 //document.write("Hello, world!");
-var A = "This is a dummy assignment!";
-window.alert(A);
+//var A = "This is a dummy assignment!";
+//window.alert(A);
+document.write("Brian told May: \"We are the champions, my friend! And no time for loosers\"<br>\"Fear of the dark!\" May responded.");
